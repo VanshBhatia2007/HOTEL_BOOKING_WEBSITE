b53aee01fbebca4de833031f9cf4407d7f67b5a4
Vansh Bhatia <168754972+VanshBhatia2007@users.noreply.github.com>
2026-01-20 00:37:20 +0530

    basic setup

--- Backend/app.js
+++ Backend/app.js
@@ -32,12 +32,24 @@ app.get("/listings",async (req,res)=>{
     const listings = await Listing.find({});
     res.render("listings/index.ejs",{listings})
 });
+//new route
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs");
+});
 
+app.post("/listings",async (req,res)=>{
+    const listing=new Listing(req.body.listing);
+    await listing.save();
+    console.log(listing);
+    res.redirect("/listings");
+});
+//specific route
 app.get("/listings/:id",async (req,res)=>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs",{listing});
 });
+
 
 app.listen(3000,()=>{
     console.log("server is listening to port 3000");
