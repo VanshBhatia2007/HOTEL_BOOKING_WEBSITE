a031be8d8201428d757ca24ca9edf864b99b6b8c
Vansh Bhatia <168754972+VanshBhatia2007@users.noreply.github.com>
2026-01-21 17:04:52 +0530

    basic setup

--- Backend/app.js
+++ Backend/app.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
-
+var methodOverride = require("method-override");
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 const Listing = require("./models/listing.js");
 
@@ -43,14 +44,32 @@ app.post("/listings",async (req,res)=>{
     console.log(listing);
     res.redirect("/listings");
 });
+
 //specific route
 app.get("/listings/:id",async (req,res)=>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs",{listing});
 });
-
-
+//edit route
+app.get("/listings/:id/edit",async (req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+});
+//update route
+app.put("/listings/:id",async (req,res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    console.log(req.body.listing);
+    res.redirect("/listings");
+});
+//Delte route
+app.delete("/listings/:id",async(req,res)=>{
+    let {id} = req.params;
+    let deletedlisting = await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+});
 app.listen(3000,()=>{
     console.log("server is listening to port 3000");
 });
--- Backend/models/listing.js
+++ Backend/models/listing.js
@@ -7,7 +7,7 @@ const listingSchema = new Schema({
         required : true,
     },
     description : String,
-    Image : {
+    image : {
         type : String,
         default : "https://images.unsplash.com/photo-1586810724476-c294fb7ac01b?q=80&w=736&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         set:(v) => v=== ""?"https://images.unsplash.com/photo-1586810724476-c294fb7ac01b?q=80&w=736&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
@@ -16,6 +16,7 @@ const listingSchema = new Schema({
     price :  {
         type : Number,
         required : true,
+        default: 0,
     },
     location : {
         type : String,
