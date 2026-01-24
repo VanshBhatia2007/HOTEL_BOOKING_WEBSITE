62b07c4aae82f4f97979cc93575b243d76cfb3e7
Vansh Bhatia <168754972+VanshBhatia2007@users.noreply.github.com>
2026-01-24 15:39:07 +0530

    basic setup

--- Backend/init/index.js
+++ Backend/init/index.js
@@ -1,25 +1,13 @@
 const mongoose = require("mongoose");
-const initdata = require("./data.js");
 const Listing = require("../models/listing.js");
-
-const MONGO_URL = "mongodb://127.0.0.1:27017/hotel_booking";
+const { data: sampleListings } = require("./data.js");
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect("mongodb://127.0.0.1:27017/hotel_booking"); // apna DB name likho
+    await Listing.deleteMany({});
+    await Listing.insertMany(sampleListings);
+    console.log("Data seeded successfully!");
+    mongoose.connection.close();
 }
 
-main()
-    .then(()=>{
-    console.log("connected to db");
-})
-    .catch((err)=>{
-        console.log(err);
-    });
-
-const initdb = async()=>{
-    await Listing.deleteMany({});
-    await Listing.insertMany(initdata.data);
-    console.log("data was init");
-};
-
-initdb();
+main().catch(console.error);
