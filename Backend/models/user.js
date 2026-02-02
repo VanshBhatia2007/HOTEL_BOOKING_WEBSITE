c1e54d7470fdccfacc4adf10b64f4dfbdf892ef3
Vansh Bhatia <168754972+VanshBhatia2007@users.noreply.github.com>
2026-02-02 17:51:57 +0530

    basic setup

---
+++ Backend/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const passportlocalmongoose = require("passport-local-mongoose");
+
+const userschema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+
+});
+
+User.plugin(passportlocalmongoose);
+
+module.exports= mongoose.model("User",userschema);
+
