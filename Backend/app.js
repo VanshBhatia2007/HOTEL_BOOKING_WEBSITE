f081c7c1dca9e280dd588ec19d30dcc33984880c
Vansh Bhatia <168754972+VanshBhatia2007@users.noreply.github.com>
2026-02-12 23:00:00 +0530

    basic setup

--- Backend/app.js
+++ Backend/app.js
@@ -75,6 +75,7 @@ app.use((req,res,next)=>{
     res.locals.success = req.flash("success");
     res.locals.error= req.flash("error");
     res.locals.curruser = req.user;
+    res.locals.MAPTILER_API_KEY = process.env.MAPTILER_API_KEY;
     console.log(res.locals.success)
     next();
 });
